dfc8f27815b20a5fd24a94acfd5610909a01b4ab
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2020-08-07 19:08:06 +0200

    update

--- plugin-config-files/development/cmake.sh
+++ plugin-config-files/development/cmake.sh
@@ -13,73 +13,75 @@
 # 3. Run cmake
 
 function eval-cmakelists-file() {
-	if grep -E -w 'project' "$1" && grep -E -w 'cmake_minimum_required' "$1"; then
-		true
-	else
-		false
-	fi
+    if grep -E -w 'project' "$1" && grep -E -w 'cmake_minimum_required' "$1"; then
+        true
+    else
+        false
+    fi
 }
 
 function build-clean() {
-	if [ -d "build" ]; then
-		echo "-- Cleaning previous build files"
-		rm -Rf "build"
-	fi
+    if [ -d "build" ]; then
+        echo "-- Cleaning previous build files"
+        rm -Rf "build"
+    fi
 }
 
 function build-generate() {
-	mkdir "build" && cd "build" || return
-	# if ! cmake .. -GNinja; then
-	# 	echo "-- Something went wrong"
-	# else
-	# 	ninja
-	# fi
-	if ! cmake .. ; then
-		echo "-- Something went wrong"
-	else
-		if ! make ; then
-		  echo "-- Failed to run make"
-		  else
-		    if ! ./bin/main
-		    echo "-- Failed to run main"
-	fi
+    mkdir "build" && cd "build" || return
+    # if ! cmake .. -GNinja; then
+    # 	echo "-- Something went wrong"
+    # else
+    # 	ninja
+    # fi
+    if ! cmake ..; then
+        echo "-- Something went wrong"
+    else
+        if ! make; then
+            echo "-- Failed to run make"
+        else
+            if ! ./bin/main; then
+                echo "-- Failed to run main"
+            fi
+        fi
+    fi
 }
 
 function build-run() {
-	if [ -d "bin" ]; then
-		cd "bin" || return
-		ls -la
-	else
-		ls -la
-	fi
-	echo "-- TODO"
+    if [ -d "bin" ]; then
+        cd "bin" || return
+        ls -la
+    else
+        ls -la
+    fi
+    echo "-- TODO"
 }
 
 function build() {
-	if ! build-clean; then
-		echo " -- Unable to clean previous build files"
-	else
-		if ! build-generate; then
-			echo "-- Unable to cmake project"
-		fi
-	fi
+    if ! build-clean; then
+        echo " -- Unable to clean previous build files"
+    else
+        if ! build-generate; then
+            echo "-- Unable to cmake project"
+        fi
+    fi
 }
 
 function walk() {
-	file="CMakeLists.txt"
-	topmost=5
+    file="CMakeLists.txt"
+    topmost=5
 
-	for ((maxwalk = topmost; maxwalk > 0; --maxwalk)); do
-		if [ -e "$PWD/$file" ]; then
-			if eval-cmakelists-file "$PWD/$file" &>/dev/null ; then
-				echo "-- Using $PWD/$file to build CXX project"
-				build
-				return
-			fi
-		fi
-		cd "../"
-	done
-	echo "-- Unable to find project CMakeLists.txt file"
+    for ((maxwalk = topmost; maxwalk > 0; --maxwalk)); do
+        if [ -e "$PWD/$file" ]; then
+            if eval-cmakelists-file "$PWD/$file" &>/dev/null; then
+                echo "-- Using $PWD/$file to build CXX project"
+                build
+                return
+            fi
+        fi
+        cd "../"
+    done
+    echo "-- Unable to find project CMakeLists.txt file"
 }
 
 walk
